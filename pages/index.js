3315b3adc83f9902ec41fc5a0cb28cf1d031ef30
khaledjamal1 <khalidfaqari@gmail.com>
2022-09-08 23:00:08 +0300

    removed modual

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+
 import hero from '../public/hero.jpg';
 import Hero from '../components/Hero';
 import Info from '../components/Info';
