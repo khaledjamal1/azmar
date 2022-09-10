535d5a0ad35daeb3a17d72f3ccd2c8ea27f7852c
khaledjamal1 <khalidfaqari@gmail.com>
2022-09-10 16:36:25 +0300

    another fix

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import Head from 'next/head';
-import Image from 'next/image';
 
-import hero from '../public/hero.jpg';
+
 import Hero from '../components/Hero';
 import Info from '../components/Info';
-import News from '../components/News';
+
 import Company from '../components/Company';
 import ContactForm from '../components/ContactForm';
 import Partners from '../components/Partners';
