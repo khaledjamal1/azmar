aea899e56dac3d773dae3aefea0433871d55fa71
khaledjamal1 <khalidfaqari@gmail.com>
2022-09-12 00:39:43 +0300

    changed bg image

--- components/Hero.js
+++ components/Hero.js
@@ -5,11 +5,11 @@ import React from 'react';
 import hero from '../public/hero.jpg';
 import { motion } from 'framer-motion';
 import { Button } from '@nextui-org/react';
-import com from '../public/com.jpg';
+import bg from '../public/bg.png';
 const Hero = () => {
   return (
     <section>
-      <div className=" text-white items-center flex h-screen">
+      <div className=" text-white  flex h-screen">
         <div className="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
           <div className="flex flex-col w-full lg:w-1/3 justify-center items-start p-8">
             <h1 className="text-3xl md:text-5xl p-2 text-text tracking-loose">
@@ -23,28 +23,8 @@ const Hero = () => {
               Contact
             </Button>
           </div>
-          <div className="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3  justify-center">
-            <div className="h-48 flex flex-wrap content-center">
-              <div>
-                <img
-                  className="inline-block mt-28 hidden xl:block"
-                  src="https://user-images.githubusercontent.com/54521023/116969935-c13d5b00-acd4-11eb-82b1-5ad2ff10fb76.png"
-                />
-              </div>
-             
-              <div>
-                <img
-                  className="inline-block mt-24 md:mt-0 p-8 md:p-0"
-                  src="https://user-images.githubusercontent.com/54521023/116969931-bedb0100-acd4-11eb-99a9-ff5e0ee9f31f.png"
-                />
-              </div>
-              <div>
-                <img
-                  className="inline-block mt-28 hidden lg:block"
-                  src="https://user-images.githubusercontent.com/54521023/116969939-c1d5f180-acd4-11eb-8ad4-9ab9143bdb50.png"
-                />
-              </div>
-            </div>
+          <div className="h-full w-[80%]">
+            <Image src={bg} alt="" layout="responsive" />
           </div>
         </div>
       </div>
