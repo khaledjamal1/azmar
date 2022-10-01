2267fc1be65355a00be083b660b53e845ca07671
khaledjamal1 <khalidfaqari@gmail.com>
2022-10-01 07:20:39 +0300

    removed gr

--- components/Hero.js
+++ components/Hero.js
@@ -6,31 +6,16 @@ import hero from '../public/hero.jpg';
 import { motion } from 'framer-motion';
 import { Button } from '@nextui-org/react';
 import bg from '../public/bg.png';
-import clouds from '../public/clouds.png';
-import clouds2 from '../public/clouds2.png';
-import Gradient from './Gradient.js';
-import dynamic from 'next/dynamic';
 
-const DynamicComponentWithNoSSR = dynamic(
-  () => import('./Gradient.js'),
-  { ssr: false } // <-- not including this component on server-side
-);
+import clouds2 from '../public/clouds2.png';
+
+
+
 const Hero = () => {
   // Create your instance
-  useEffect(() => {
-    const gradient = new Gradient();
-
-    // Call `initGradient` with the selector to your canvas
-    gradient.initGradient('#gradient-canvas');
-  }, []);
+  
   return (
     <section>
-            <canvas
-              id="gradient-canvas"
-              style={{ width: '100vw', height: '70vh',backgroundColor: '#06070d' }}
-              className='absolute opacity-10 overflow-hidden  skew-y-12 -z-1'
-              data-transition-in
-            ></canvas>
       <div className=" text-white  flex sm:h-[70vh] w-screen h-screen">
         <div className="container mx-auto flex flex-col-reverse md:flex-row items-center justify-between my-12 md:my-14">
           <div className="flex flex-col w-full lg:w-1/3 justify-center md:items-start p-3">
